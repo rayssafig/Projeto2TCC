dce4597b4ac5118cafc4c4c1f374d9d2bd134cc3
Rayssa <rayssafime@gmail.com>
2022-03-13 15:26:20 -0300

    Updating multiple apps

--- apps/agua.py
+++ apps/agua.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-import pandas_profiling
-from streamlit_pandas_profiling import st_profile_report
 
 def app():
     global data
@@ -13,7 +11,3 @@ def app():
     #st.image('https://github.com/rayssafig/Projeto2TCC/tree/master/data/Irai_GIF.gif')
     st.image('C:/Users/rayss/PycharmProjects/Projeto2TCC/apps/Irai_GIF.gif')
 
-    df = pd.read_csv("https://storage.googleapis.com/tf-datasets/titanic/train.csv")
-    pr = df.profile_report()
-
-    st_profile_report(pr)
